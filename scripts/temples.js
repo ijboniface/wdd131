9e0d90b9149ac1d3503edb9e99e224bda1f29e35
ijboniface <iboniface@byupathway.edu>
2026-07-11 11:34:24 +0100

    temples.html

---
+++ scripts/temples.js
@@ -0,0 +1,13 @@
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    menuButton.classList.toggle("open");
+});
+
+document.querySelector("#currentyear").textContent =
+    new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
