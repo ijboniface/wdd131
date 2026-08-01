57c3b66dc82d89151238c5b5e881ecedc9d1e653
ijboniface <iboniface@byupathway.edu>
2026-08-01 09:03:03 +0100

    form.html

---
+++ scripts/form.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+        id: "fc-1888",
+        name: "flux capacitor",
+        averageRating: 4.5
+    },
+    {
+        id: "fc-2050",
+        name: "power laces",
+        averageRating: 4.7
+    },
+    {
+        id: "fs-1987",
+        name: "time circuits",
+        averageRating: 3.5
+    },
+    {
+        id: "ac-2000",
+        name: "low voltage reactor",
+        averageRating: 3.9
+    },
+    {
+        id: "jj-1969",
+        name: "warp equalizer",
+        averageRating: 5.0
+    }
+];
+
+const productSelect = document.querySelector("#product");
+
+products.forEach((product) => {
+    const option = document.createElement("option");
+
+    option.value = product.id;
+    option.textContent = product.name;
+
+    productSelect.appendChild(option);
+});
+
+document.querySelector("#currentyear").textContent =
+    new Date().getFullYear();
+
+document.querySelector("#lastModified").textContent =
+    `Last Modified: ${document.lastModified}`;
